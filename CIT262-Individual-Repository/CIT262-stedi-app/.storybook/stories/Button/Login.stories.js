2f59d75c9205829db6e3c96ec607d9fdbf320004
Christian <crissbritez70@gmail.com>
2024-02-21 13:53:39 -0300

    updates

---
+++ CIT262-Individual-Repository/CIT262-stedi-app/.storybook/stories/Button/Login.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import Login from '../path/to/Login';
+
+storiesOf('Login', module)
+  .add('default', () => <Login />);
